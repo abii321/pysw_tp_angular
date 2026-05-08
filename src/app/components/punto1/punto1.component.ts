2b1ab60c497ac9f002756bb0c82b78592dea38b3
lujancansino <celestelujancansino@gmail.com>
2026-05-08 15:38:50 -0300

    punto 1 arreglos esteticos-Lujan Cansino

--- src/app/components/punto1/punto1.component.ts
+++ src/app/components/punto1/punto1.component.ts
@@ -1,31 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Punto1Service } from '../../services/punto1.service'; // Importamos el servicio
 
 @Component({
   selector: 'app-punto1',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './punto1.component.html',
-  styleUrl: './punto1.component.css',
+  styleUrl: './punto1.component.css'
 })
 export class Punto1Component {
-
-  eventos: Array<any> = [
-    { nombre: 'Yoga', descripcion: 'Clase relajante', img: 'yoga.jpg' },
-    { nombre: 'Música', descripcion: 'Concierto en vivo', img: 'musica.jpg' }
-  ];
-
+  eventos: Array<any> = []; // empieza vacío
   indice: number = 0;
 
+  // Inyectamos el servicio en el constructor
+  constructor(private punto1Service: Punto1Service) {
+    // Le pedimos los datos al servicio al iniciar
+    this.eventos = this.punto1Service.getEventos();
+  }
+
   siguiente() {
-    // Solo sumamos si NO estamos en el último elemento
     if (this.indice < this.eventos.length - 1) {
       this.indice++;
     }
   }
 
   anterior() {
-    // Solo restamos si NO estamos en el primero
     if (this.indice > 0) {
       this.indice--;
     }
